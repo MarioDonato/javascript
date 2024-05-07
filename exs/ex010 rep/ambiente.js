cd0bed91277f8bdf39863d16bb0407d39feb4df5
MarioDonato <167766433+MarioDonato@users.noreply.github.com>
2024-05-07 07:50:25 -0300

    for

--- exs/ex010 rep/ambiente.js
+++ exs/ex010 rep/ambiente.js
@@ -1,6 +1,16 @@
 var c = 1
 
-while ( c <= 6) {
+/*while ( c <= 6) {
     console.log(`Tudo Bem? falei ${c} vezes`)
     c++
+}*/
+
+
+/*do {
+    console.log(`Ola mundo, falei ${c} vezes`)
+    c++
+} while (c <= 6)*/
+
+for (var c = 1; c <= 2; c++){
+    console.log(`tudo bem?, ${c} com o for`)
 }
